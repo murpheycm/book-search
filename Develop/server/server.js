97345d29d4c7e86230bed018b5e0bd3eb07eb2c8
murpheycm <cmmurphey@gmail.com>
2023-11-02 05:15:07 -0400

    Updated packages and middleware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,11 +3,31 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
