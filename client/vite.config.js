e327667efbf88363a387aa91e83f6045a9d75524
murpheycm <cmmurphey@gmail.com>
2023-11-03 11:33:49 -0400

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
